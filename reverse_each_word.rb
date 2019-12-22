1d0e202e71c37b7ab4eb7984258eba4609dfd3ae
Allen Phillip Pottinger <allenpottinger@gmail.com>
2019-12-22 00:32:17 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+reverse_each_word("Hello there, and how are you?")
+"Hello there, and how are you?".split
+
+def words(in_reverse)
+  words.collect do |word_in_reverse|
+    puts "#{in_reverse}
+  end
+end
