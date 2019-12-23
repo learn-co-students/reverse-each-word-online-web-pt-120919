a9bf30d6d278dd1963f3de1983bed064c72a9ad9
travis Lawson <travislawclimb@gmail.com>
2019-12-23 00:06:35 +0000

    Done.

--- spec/reverse_each_word_spec.rb
+++ spec/reverse_each_word_spec.rb
@@ -1,18 +1,17 @@
-require 'spec_helper'
-
-describe '#reverse_each_word' do
-  let(:sentence1) { "Hello there, and how are you?" }
-  it 'reverses all the words in a string without reversing the order of the words' do
-    expect(reverse_each_word(sentence1)).to eq("olleH ,ereht dna woh era ?uoy")
+def reverse_each_word_with_each(string)
+    original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
   end
+  return_array.join(" ")
+end
 
-  let(:sentence2) { "Hi again, just making sure it's reversed!" }
-  it 'reverses all the words in another string without reversing the order of the words' do
-    expect(reverse_each_word(sentence2)).to eq("iH ,niaga tsuj gnikam erus s'ti !desrever")
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
   end
-
-  it 'uses collect' do
-    expect_any_instance_of(Array).to receive(:collect).and_call_original
-    reverse_each_word("Verifying that collect is being called.")
-  end
+  test_array.join(" ")
 end
